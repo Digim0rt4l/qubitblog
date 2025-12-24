5aa62d49e6302bb24ee4829e52f09bbb38eb7ab8
Scott Forbes <flakefrost@gmail.com>
2025-12-24 20:30:59 +1030

    Update sitemap.xml.11ty.js

--- pages/sitemap.xml.11ty.js
+++ pages/sitemap.xml.11ty.js
@@ -2,7 +2,8 @@ module.exports = class {
 	data() {
 		return {
 			permalink: "/sitemap.xml",
-			eleventyExcludeFromCollections: true
+			eleventyExcludeFromCollections: true,
+			layout: false
 		};
 	}
 
@@ -12,19 +13,39 @@ module.exports = class {
 		const urls = collections.all
 			.map(page => page.url)
 			.filter(Boolean)
-			.filter(url =>
-				!url.startsWith("/authors/") &&
-				!url.startsWith("/tags/") &&
-				!url.startsWith("/admin/") &&
-				url !== "/404/" &&
-				url !== "/404.html"
+			.map(url => {
+				if (url.startsWith("http")) {
+					try {
+						return new URL(url).pathname;
+					} catch {
+						return url;
+					}
+				}
+				return url;
+			})
+			.map(path => {
+				let p = path.startsWith("/") ? path : `/${path}`;
+				while (p.startsWith("//")) p = p.slice(1);
+				return p;
+			})
+			.filter(path =>
+				!path.startsWith("/authors/") &&
+				!path.startsWith("/tags/") &&
+				!path.startsWith("/admin/") &&
+				path !== "/404/" &&
+				path !== "/404.html"
 			);
 
 		return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls.map(url => `	<url>
-		<loc>${baseUrl}${url}</loc>
-	</url>`).join("\n")}
-</urlset>`;
+${urls
+	.map(
+		path => `	<url>
+		<loc>${baseUrl}${path}</loc>
+	</url>`
+	)
+	.join("\n")}
+</urlset>
+`;
 	}
 };
