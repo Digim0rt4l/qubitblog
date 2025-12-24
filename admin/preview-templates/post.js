811b5c8c6301da9984cf662d4f82e74700bf7d3c
Scott Forbes <flakefrost@gmail.com>
2025-12-24 18:18:01 +1030

    Update post.js

--- admin/preview-templates/post.js
+++ admin/preview-templates/post.js
@@ -3,46 +3,63 @@ import format from "https://unpkg.com/date-fns@2.7.0/esm/format/index.js?module"
 
 const html = htm.bind(h);
 
+function safeFormatDate(value) {
+	if (!value) {
+		return "";
+	}
+
+	const date = value instanceof Date ? value : new Date(value);
+
+	if (Number.isNaN(date.getTime())) {
+		return "";
+	}
+
+	return format(date, "dd MMM, yyyy");
+}
+
 // Preview component for a Post
 const Post = createClass({
-  render() {
-    const entry = this.props.entry;
+	render() {
+		const entry = this.props.entry;
 
-    return html`
-      <main>
-        <article>
-          <h1>${entry.getIn(["data", "title"], null)}</h1>
-          <p>
-            <small>
-              <time
-                >${
-                  format(
-                    entry.getIn(["data", "date"], new Date()),
-                    "dd MMM, yyyy"
-                  )
-                }</time
-              >
-              ${" by Author"}
-            </small>
-          </p>
+		const title = entry.getIn(["data", "title"], null);
+		const dateValue = entry.getIn(["data", "date"], null);
+		const dateText = safeFormatDate(dateValue);
+		const summary = entry.getIn(["data", "summary"], "");
+		const tags = entry.getIn(["data", "tags"], []);
 
-          <p>${entry.getIn(["data", "summary"], "")}</p>
+		return html`
+			<main>
+				<article>
+					<h1>${title}</h1>
 
-          ${this.props.widgetFor("body")}
-          <p>
-            ${
-              entry.getIn(["data", "tags"], []).map(
-                tag =>
-                  html`
-                    <a href="#" rel="tag">${tag}</a>
-                  `
-              )
-            }
-          </p>
-        </article>
-      </main>
-    `;
-  }
+					<p>
+						<small>
+							${dateText
+								? html`
+										<time>${dateText}</time>
+									`
+								: null}
+							${" by Author"}
+						</small>
+					</p>
+
+					<p>${summary}</p>
+
+					${this.props.widgetFor("body")}
+
+					<p>
+						${tags.map(
+							tag =>
+								html`
+									<a href="#" rel="tag">${tag}</a>
+								`
+						)}
+					</p>
+				</article>
+			</main>
+		`;
+	}
 });
 
 export default Post;
